525f86cf04a2985e61add02505722f50a22db5c9
PEPPACHAN <PEPPACHAN@users.noreply.github.com>
2024-06-28 15:50:24 +0300

    Initial Commit

---
+++ mapPet/mapPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapPetApp.swift
+//  mapPet
+//
+//  Created by PEPPA CHAN on 28.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct mapPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
